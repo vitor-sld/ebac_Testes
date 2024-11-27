6e37532751c8ab49d88bbf94e163a7a267ea8440
Vitor Augusto Costa Teles <48792332+vitor-sld@users.noreply.github.com>
2024-11-27 16:51:01 -0300

    First commit

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,33 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve postar 2 comentários', () =>  {
+        render(<PostComment />)
+
+        const textareaElement = screen.getByTestId('txt-comment')
+        const buttonElement = screen.getByTestId('btn-submit')
+
+        // comentario 1
+        fireEvent.change(textareaElement, {
+            target: {
+                value: 'Testando o primeiro comentario'
+            }
+        })
+        fireEvent.click(buttonElement)
+
+        // comentario 2
+        fireEvent.change(textareaElement, {
+            target: {
+                value: 'Testando o segundo comentario'
+            }
+        })
+        fireEvent.click(buttonElement)
+
+        // const comentario = screen.getTestById('comentario')
+        const comentario = screen.getAllByTestId('comentario')
+
+        // console.log(comentario.length)
+        expect(comentario).toHaveLength(2)
+    })
 });
